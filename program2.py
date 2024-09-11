6affb3939c75bd18b51d3d00fae2dfff2c588f1b
Prasiddhi-upd <upadhyaprasiddhi02@gmail.com>
2024-09-10 22:18:28 -0500

    Added program2.py in the branch new_feature

---
+++ program2.py
@@ -0,0 +1,8 @@
+print("Hello World! This is program2.")
+print("Let's calculate cube roots.")
+
+num = 8
+
+num_cubert = num ** (1/3)
+
+print ("The cube root of " + str(num) + " is " + str(num_cubert) + ".")
