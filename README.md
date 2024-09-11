e7f0ca89a0f67f584cd5c1b349e567e2eaa5cfdd
Prasiddhi-upd <144473681+Prasiddhi-upd@users.noreply.github.com>
2024-09-10 21:39:50 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+README
+
+Prasiddhi Upadhyay
+Student id: 800740623
+
+Assignment 1
+program1.py == Square root calculator
+program2.py == Cube root calculator
---
+++ program1.py
@@ -0,0 +1,8 @@
+print ("Hello world!")
+print ("Let's calculate square roots.")
+
+num = 16
+
+num_sqrt = num ** 0.5
+
+print ("The square root of " + str(num) + " is " + str(num_sqrt) + ".")
